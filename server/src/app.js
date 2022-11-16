00f69a0fc2caf45427f103f1e48906f13bb54da9
ralph <wealthralph@gmail.com>
2022-11-16 07:39:00 +0100

    start

---
+++ server/src/app.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const passport = require('passport')
+const GoogleStrategy = require("passport-google-oauth20").Strategy
+const app = express()
+
+passport.use(new GoogleStrategy())
+
+module.exports = app
