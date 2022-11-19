a8e3fe84581678d53a69563019126a57c190965e
ralph <wealthralph@gmail.com>
2022-11-19 13:15:18 +0100

    routes update

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,6 +1,11 @@
 const passport = require("passport");
 
 module.exports = (app) => {
+
+  app.get('/', (req, res) => {
+    res.send({json: " motherfucker"})
+  })
+
   app.get(
     "/auth/google",
     passport.authenticate("google", { scope: ["profile", "email"] })
